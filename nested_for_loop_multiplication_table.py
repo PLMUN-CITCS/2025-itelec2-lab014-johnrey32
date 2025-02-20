56a1eb3cb72c81768d19cbb23aa960ca2e0ba71b
johnrey32 <bulfajohnrey_bsit@plmun.edu.ph>
2025-02-20 11:02:48 +0800

    Submitting Python Week 04 - Laboratory # 14

---
+++ nested_for_loop_multiplication_table.py
@@ -0,0 +1,14 @@
+# John Rey Bulfa
+# ITELEC2
+# Laboratory #14 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+    for i in range(1, 6):
+        for j in range(1, 6):  # Indent this loop!
+            product = i * j  # Indent this line!
+            print(f"{product:4}", end="")  # Indent this line!
+        print() # Indent this line!
+
+if __name__ == "__main__":
+    main()
